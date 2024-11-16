06b95b89c3a0697f54cbc3ceaef7dccd6261c081
ytgw <tagawa0920@gmail.com>
2024-11-16 09:51:30 +0000

    練習問題1.2の回答を追加

---
+++ chapter01/section02/practice01/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+void hello_world()
+{
+    std::cout << "hello, world" << std::endl;
+}
+
+
+
+int main()
+{
+    hello_world();
+}
---
+++ chapter01/section02/practice02/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int sum(int a, int b, int c)
+{
+    return a + b + c;
+}
+
+
+
+int main()
+{
+    std::cout << "sum(5, 1, -2): " << sum(5, 1, -2) << std::endl;
+}
