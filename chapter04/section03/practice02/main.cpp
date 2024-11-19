ecba3cc1dd37aae2e06793ffbd18ef7c7f81e506
ytgw <tagawa0920@gmail.com>
2024-11-19 13:56:48 +0000

    練習問題4.3の回答を追加

---
+++ chapter04/section03/practice02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int get_counter()
+{
+    static int counter = 0;
+    return counter++;
+}
+
+
+int main()
+{
+    std::cout << get_counter() << std::endl;
+    std::cout << get_counter() << std::endl;
+    std::cout << get_counter() << std::endl;
+}
