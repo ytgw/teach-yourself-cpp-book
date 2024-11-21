93ea36e24434ade3ce31371d8bd04d2f062ca0ba
ytgw <tagawa0920@gmail.com>
2024-11-21 14:11:32 +0000

    練習問題5.3の回答を追加

---
+++ chapter05/section03/practice02/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+
+class Example
+{
+public:
+    Example() { std::cout << "constructor" << std::endl; }
+    ~Example() { std::cout << "destructor" << std::endl; }
+};
+
+
+int main()
+{
+    Example* ex = new Example();
+    std::cout << "main" << std::endl;
+    // delete ex;  // コメントアウトすると終了時にもデストラクタが呼ばれなかった
+}
