013e4b02ddd637658d6586abadd143e52e9278be
ytgw <tagawa0920@gmail.com>
2024-11-22 08:30:57 +0000

    練習問題6.2の回答を追加

---
+++ chapter06/section02/practice01/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+class Float
+{
+private:
+    float value;
+
+public:
+    Float(float value): value(value) {}
+    void show() const { std::cout << value << std::endl; }
+
+    Float operator+(const Float& rhs) const { return Float{value + rhs.value}; }
+    Float operator-(const Float& rhs) const { return Float{value - rhs.value}; }
+};
+
+
+int main()
+{
+    Float a = 1.5f;
+    Float b = 2.25f;
+
+    (a + b).show();
+    (a - b).show();
+}
