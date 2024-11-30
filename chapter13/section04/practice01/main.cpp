672093feb2c2d284851bbf106315a2b24667ceea
ytgw <tagawa0920@gmail.com>
2024-11-30 09:23:58 +0300

    練習問題13.4の回答を追加

---
+++ chapter13/section04/practice01/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main()
+{
+    std::string str = std::to_string(123);
+    std::cout << str << std::endl;
+}
---
+++ chapter13/section04/practice02/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main()
+{
+    const double pi = std::stod("3.14159265");
+    std::cout << pi << std::endl;
+}
